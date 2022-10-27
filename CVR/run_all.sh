ed18162c93ad1f8de77da41759fa9dde3bfdbb49
Michael Lepori <mlepori@login005.oscar.ccv.brown.edu>
2022-10-27 19:03:27 -0400

    added test run scripts

--- CVR/run_all.sh
+++ CVR/run_all.sh
@@ -1,9 +1,10 @@
 export PROJECT_DIR=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks
-for file in configs/CompositionalSubnetworks/RN18_Param_Search/Inside_Subnetwork/Train/*;
+for file in configs/CompositionalSubnetworks/RN50_Param_Search/Inside_Subnetwork/Train0/*;
 do
     echo "$file"
     export JOBNAME=$(basename "$file" .yaml)
-    export CONFIG=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks/CVR/configs/CompositionalSubnetworks/RN18_Param_Search/Inside_Subnetwork/Train/${JOBNAME}.yaml
+    export CONFIG=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks/CVR/configs/CompositionalSubnetworks/RN50_Param_Search/Inside_Subnetwork/Train0/${JOBNAME}.yaml
+
     sbatch -J CS-$JOBNAME $PROJECT_DIR/CVR/run.script
 done
 
---
+++ CVR/run_tests.sh
@@ -0,0 +1,10 @@
+export PROJECT_DIR=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks
+for file in configs/CompositionalSubnetworks/RN50_Param_Search/Inside_Subnetwork/Test/*;
+do
+    echo "$file"
+    export JOBNAME=$(basename "$file" .yaml)
+    export CONFIG=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks/CVR/configs/CompositionalSubnetworks/RN50_Param_Search/Inside_Subnetwork/Test/${JOBNAME}.yaml
+
+    sbatch -J CS-$JOBNAME $PROJECT_DIR/CVR/test.script
+done
+
