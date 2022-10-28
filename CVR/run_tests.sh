8f4ef11fc877579294438c5556d3e62f767547a0
Michael Lepori <mlepori@login005.oscar.ccv.brown.edu>
2022-10-27 20:29:56 -0400

    fixed test script

--- CVR/run_tests.sh
+++ CVR/run_tests.sh
@@ -1,9 +1,9 @@
 export PROJECT_DIR=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks
-for file in configs/CompositionalSubnetworks/RN50_Param_Search/Inside_Subnetwork/Test/*;
+for file in configs/CompositionalSubnetworks/RN50_Param_Search/Contact_Subnetwork/Test/*;
 do
     echo "$file"
     export JOBNAME=$(basename "$file" .yaml)
-    export CONFIG=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks/CVR/configs/CompositionalSubnetworks/RN50_Param_Search/Inside_Subnetwork/Test/${JOBNAME}.yaml
+    export CONFIG=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks/CVR/configs/CompositionalSubnetworks/RN50_Param_Search/Contact_Subnetwork/Test/${JOBNAME}.yaml
 
     sbatch -J CS-$JOBNAME $PROJECT_DIR/CVR/test.script
 done
