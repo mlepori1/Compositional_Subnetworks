cf703ff7db9d6766c355d8bfaef43f788b264412
Michael Lepori <mlepori19@gmail.com>
2022-10-30 22:30:38 -0400

    analyze mlp too

--- CVR/analyze_masks.py
+++ CVR/analyze_masks.py
@@ -44,6 +44,7 @@ def cli_main():
                 l0_init=0,
                 l0_lambda=0.000000001)
 
+    print("CNN L4")
     cnn1_l4 = list(cnn1.backbone.layer4.modules())
     cnn2_l4 = list(cnn2.backbone.layer4.modules())
     for layer_idx in range(len(cnn1_l4)):
@@ -59,6 +60,22 @@ def cli_main():
             print("Mask intesection: ", torch.logical_and(mask1, mask2).sum())
             print("Size of tensor: ", mask1.reshape(-1).size())
 
+    print("MLP")
+    cnn1_mlp = list(cnn1.mlp.modules())
+    cnn2_mlp = list(cnn2.mlp.modules())
+    for layer_idx in range(len(cnn1_mlp)):
+        layer1 = cnn1_mlp[layer_idx]
+        if isinstance(layer1, L0Conv2d):
+            layer2 = cnn2_mlp[layer_idx]
+
+            mask1 = layer1.compute_mask()
+            mask2 = layer2.compute_mask()
+
+            print("Mask1 params: ", mask1.sum())
+            print("Mask2 params: ", mask2.sum())
+            print("Mask intesection: ", torch.logical_and(mask1, mask2).sum())
+            print("Size of tensor: ", mask1.reshape(-1).size())
+            
 if __name__ == '__main__':
     print(os.getpid())
 
