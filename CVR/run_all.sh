7d116efedf9f8775a5b45e7b48400b5ae5c9d4e0
Michael Lepori <mlepori@node1102.oscar.ccv.brown.edu>
2022-11-11 15:35:43 -0500

    added config files

--- CVR/run_all.sh
+++ CVR/run_all.sh
@@ -1,9 +1,9 @@
 export PROJECT_DIR=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks
-for file in configs/CompositionalSubnetworks/RN50_Contact_Inside/*;
+for file in configs/CompositionalSubnetworks/WRN50_Contact_Inside/*;
 do
     echo "$file"
     export JOBNAME=$(basename "$file" .yaml)
-    export CONFIG=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks/CVR/configs/CompositionalSubnetworks/RN50_Contact_Inside/${JOBNAME}.yaml
+    export CONFIG=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks/CVR/configs/CompositionalSubnetworks/WRN50_Contact_Inside/${JOBNAME}.yaml
 
  
     sbatch -J CS-$JOBNAME -o out/${JOBNAME}.out -e err/${JOBNAME}.err $PROJECT_DIR/CVR/run.script
