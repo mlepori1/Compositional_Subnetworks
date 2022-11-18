8b596060c5d2864c3b2383709060bb86dd424472
Michael Lepori <mlepori@gpu1204.oscar.ccv.brown.edu>
2022-11-18 14:28:45 -0500

    synch

--- CVR/check_mask.py
+++ CVR/check_mask.py
@@ -1,4 +1,7 @@
 import torch
 
-t = torch.load("/users/mlepori/data/mlepori/projects/Compositional_Subnetworks/Models/resnet50/Contact_Inside_Experiments/test/1_mlp.pt_mask")
+t = torch.load("/users/mlepori/data/mlepori/projects/Compositional_Subnetworks/Models/resnet50/Contact_Inside_Experiments/test/1_mlp.pt_none_mask")
 print(t.sum())
+t = torch.load("/users/mlepori/data/mlepori/projects/Compositional_Subnetworks/Models/resnet50/Contact_Inside_Experiments/test/1_mlp.pt_zero_mask")
+print(t.sum())
+
--- CVR/run_all.sh
+++ CVR/run_all.sh
@@ -1,9 +1,9 @@
 export PROJECT_DIR=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks
-for file in configs/CompositionalSubnetworks/RN50_Contact_Inside/Experiment_Configs/*;
+for file in configs/CompositionalSubnetworks/RN50_Contact_Inside/test/*;
 do
     echo "$file"
     export JOBNAME=$(basename "$file" .yaml)
-    export CONFIG=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks/CVR/configs/CompositionalSubnetworks/RN50_Contact_Inside/Experiment_Configs/${JOBNAME}.yaml
+    export CONFIG=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks/CVR/configs/CompositionalSubnetworks/RN50_Contact_Inside/test/${JOBNAME}.yaml
 
  
     sbatch -J CS-$JOBNAME -o out/${JOBNAME}.out -e err/${JOBNAME}.err $PROJECT_DIR/CVR/run.script
