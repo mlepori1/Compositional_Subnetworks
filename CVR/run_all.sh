4338f6569ed0475bf77243cc2ef776d711ecacad
Michael Lepori <mlepori19@gmail.com>
2022-11-18 22:28:46 -0500

    edited runall

--- CVR/run_all.sh
+++ CVR/run_all.sh
@@ -1,9 +1,9 @@
 export PROJECT_DIR=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks
-for file in configs/CompositionalSubnetworks/RN50_Contact_Inside/Experiment_Configs/*;
+for file in configs/CompositionalSubnetworks/RN50_Contact_Inside/Exp_Tests/*;
 do
     echo "$file"
     export JOBNAME=$(basename "$file" .yaml)
-    export CONFIG=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks/CVR/configs/CompositionalSubnetworks/RN50_Contact_Inside/Experiment_Configs/${JOBNAME}.yaml
+    export CONFIG=/gpfs/data/epavlick/mlepori/projects/Compositional_Subnetworks/CVR/configs/CompositionalSubnetworks/RN50_Contact_Inside/Exp_Tests/${JOBNAME}.yaml
 
  
     sbatch -J CS-$JOBNAME -o out/${JOBNAME}.out -e err/${JOBNAME}.err $PROJECT_DIR/CVR/run.script
