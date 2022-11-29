259efb1ea4f2ca9baacf951cb8ababfbc31a70c6
Michael Lepori <mlepori19@gmail.com>
2022-11-29 12:40:21 -0500

    RN50 train Inside count

--- CVR/run_all.sh
+++ CVR/run_all.sh
@@ -1,9 +1,9 @@
 export PROJECT_DIR=/users/mlepori/scratch/Compositional_Subnetworks
-for file in configs/CompositionalSubnetworks/Train_Contact_Inside/*;
+for file in configs/CompositionalSubnetworks/Train_Inside_Count/*;
 do
     echo "$file"
     export JOBNAME=$(basename "$file" .yaml)
-    export CONFIG=/users/mlepori/scratch/Compositional_Subnetworks/CVR/configs/CompositionalSubnetworks/Train_Contact_Inside/${JOBNAME}.yaml
+    export CONFIG=/users/mlepori/scratch/Compositional_Subnetworks/CVR/configs/CompositionalSubnetworks/Train_Inside_Count/${JOBNAME}.yaml
 
  
     sbatch -J CS-$JOBNAME -o out/${JOBNAME}.out -e err/${JOBNAME}.err $PROJECT_DIR/CVR/run.script
