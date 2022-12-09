e22ce4d5a6c7c189c3aaec8a985ddca69ff623dc
Michael Lepori <mlepori19@gmail.com>
2022-12-09 17:24:02 -0500

    bert hyperparameter sweep sv agr s

--- Language/SyntacticSubnetworks/run_all.sh
+++ Language/SyntacticSubnetworks/run_all.sh
@@ -1,9 +1,9 @@
 export PROJECT_DIR=/users/mlepori/data/mlepori/projects/Compositional_Subnetworks/
-for file in configs/CompositionalSubnetworks/test/*;
+for file in configs/CompositionalSubnetworks/SV_Agreement_Singular_Hyperparameter_Sweep/*;
 do
     echo "$file"
     export JOBNAME=$(basename "$file" .yaml)
-    export CONFIG=/users/mlepori/data/mlepori/projects/Compositional_Subnetworks/Language/SyntacticSubnetworks/configs/CompositionalSubnetworks/test/${JOBNAME}.yaml
+    export CONFIG=/users/mlepori/data/mlepori/projects/Compositional_Subnetworks/Language/SyntacticSubnetworks/configs/CompositionalSubnetworks/SV_Agreement_Singular_Hyperparameter_Sweep/${JOBNAME}.yaml
 
  
     sbatch -J CS-$JOBNAME -o out/${JOBNAME}.out -e err/${JOBNAME}.err $PROJECT_DIR/Language/SyntacticSubnetworks/run.script
